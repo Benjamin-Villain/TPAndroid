8e0b5cbc4a484802ff5ed9cb12ffe91c8a678b6f
Benjamin-Villain <benjaminnicolasvillain@gmail.com>
2020-10-25 21:05:08 +0100

    implémentation de l'adaptateur

--- app/src/main/java/com/example/tpandroid/MainActivity.kt
+++ app/src/main/java/com/example/tpandroid/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val andVersionArray = arrayOf(AndVersion("Banana"), AndVersion("Lollipop"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
