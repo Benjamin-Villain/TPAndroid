74642e5233d34cb0d06e97c785f9119d57b239fa
Benjamin-Villain <benjaminnicolasvillain@gmail.com>
2020-10-25 21:10:16 +0100

    Configuration de l’adaptateur avec sa vue

--- app/src/main/java/com/example/tpandroid/MainActivity.kt
+++ app/src/main/java/com/example/tpandroid/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.tpandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        andVersionRecyclerView.layoutManager = LinearLayoutManager(this)
+        andVersionRecyclerView.adapter = AndVersionAdapter(andVersionArray)
     }
 }
